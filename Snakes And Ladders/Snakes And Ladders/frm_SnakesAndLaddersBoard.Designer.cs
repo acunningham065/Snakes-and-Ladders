93f82fb848624efa1e1cf75af79b505ec9b5f626
acunningham065 <acunningham065@gmail.com>
2016-04-03 20:02:45 +0100

    Add files via Upload

---
+++ Snakes And Ladders/Snakes And Ladders/frm_SnakesAndLaddersBoard.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Snakes_And_Ladders
+{
+    partial class frm_SnakesAndLaddersBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_SnakesAndLaddersBoard));
+            this.btn_rollDie = new System.Windows.Forms.Button();
+            this.lbl_winnerTitle = new System.Windows.Forms.Label();
+            this.lbl_winnerIs = new System.Windows.Forms.Label();
+            this.lbl_PlayerThatIsGoing = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.pbx_Diceface = new System.Windows.Forms.PictureBox();
+            this.pnl_board = new System.Windows.Forms.Panel();
+            this.pbx_Player2 = new System.Windows.Forms.PictureBox();
+            this.pbx_Player1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Diceface)).BeginInit();
+            this.pnl_board.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Player1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_rollDie
+            // 
+            this.btn_rollDie.Enabled = false;
+            this.btn_rollDie.Location = new System.Drawing.Point(42, 557);
+            this.btn_rollDie.Name = "btn_rollDie";
+            this.btn_rollDie.Size = new System.Drawing.Size(180, 70);
+            this.btn_rollDie.TabIndex = 1;
+            this.btn_rollDie.Text = "Roll Die";
+            this.btn_rollDie.UseVisualStyleBackColor = true;
+            this.btn_rollDie.Click += new System.EventHandler(this.btn_rollDie_Click);
+            // 
+            // lbl_winnerTitle
+            // 
+            this.lbl_winnerTitle.AutoSize = true;
+            this.lbl_winnerTitle.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_winnerTitle.Location = new System.Drawing.Point(30, 27);
+            this.lbl_winnerTitle.Name = "lbl_winnerTitle";
+            this.lbl_winnerTitle.Size = new System.Drawing.Size(201, 31);
+            this.lbl_winnerTitle.TabIndex = 2;
+            this.lbl_winnerTitle.Text = "The winner is ...";
+            this.lbl_winnerTitle.Visible = false;
+            // 
+            // lbl_winnerIs
+            // 
+            this.lbl_winnerIs.AutoSize = true;
+            this.lbl_winnerIs.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_winnerIs.Location = new System.Drawing.Point(30, 58);
+            this.lbl_winnerIs.Name = "lbl_winnerIs";
+            this.lbl_winnerIs.Size = new System.Drawing.Size(90, 31);
+            this.lbl_winnerIs.TabIndex = 3;
+            this.lbl_winnerIs.Text = "Player";
+            this.lbl_winnerIs.Visible = false;
+            // 
+            // lbl_PlayerThatIsGoing
+            // 
+            this.lbl_PlayerThatIsGoing.AutoSize = true;
+            this.lbl_PlayerThatIsGoing.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_PlayerThatIsGoing.Location = new System.Drawing.Point(37, 520);
+            this.lbl_PlayerThatIsGoing.Name = "lbl_PlayerThatIsGoing";
+            this.lbl_PlayerThatIsGoing.Size = new System.Drawing.Size(0, 27);
+            this.lbl_PlayerThatIsGoing.TabIndex = 4;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(42, 103);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(180, 70);
+            this.btn_Start.TabIndex = 5;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // pbx_Diceface
+            // 
+            this.pbx_Diceface.Location = new System.Drawing.Point(42, 324);
+            this.pbx_Diceface.Name = "pbx_Diceface";
+            this.pbx_Diceface.Size = new System.Drawing.Size(180, 180);
+            this.pbx_Diceface.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_Diceface.TabIndex = 6;
+            this.pbx_Diceface.TabStop = false;
+            // 
+            // pnl_board
+            // 
+            this.pnl_board.BackgroundImage = global::Snakes_And_Ladders.Properties.Resources.snakes_and_ladders_game_board_by_avaruusturri_d63fn1g;
+            this.pnl_board.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnl_board.Controls.Add(this.pbx_Player2);
+            this.pnl_board.Controls.Add(this.pbx_Player1);
+            this.pnl_board.Location = new System.Drawing.Point(269, 27);
+            this.pnl_board.Name = "pnl_board";
+            this.pnl_board.Size = new System.Drawing.Size(600, 600);
+            this.pnl_board.TabIndex = 0;
+            // 
+            // pbx_Player2
+            // 
+            this.pbx_Player2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pbx_Player2.Location = new System.Drawing.Point(25, 564);
+            this.pbx_Player2.Name = "pbx_Player2";
+            this.pbx_Player2.Size = new System.Drawing.Size(20, 20);
+            this.pbx_Player2.TabIndex = 2;
+            this.pbx_Player2.TabStop = false;
+            this.pbx_Player2.Visible = false;
+            // 
+            // pbx_Player1
+            // 
+            this.pbx_Player1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.pbx_Player1.Location = new System.Drawing.Point(25, 538);
+            this.pbx_Player1.Name = "pbx_Player1";
+            this.pbx_Player1.Size = new System.Drawing.Size(20, 20);
+            this.pbx_Player1.TabIndex = 0;
+            this.pbx_Player1.TabStop = false;
+            this.pbx_Player1.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "apple.png");
+            this.imageList2.Images.SetKeyName(1, "banana.png");
+            this.imageList2.Images.SetKeyName(2, "kiwi.jpg");
+            this.imageList2.Images.SetKeyName(3, "lemon.jpg");
+            this.imageList2.Images.SetKeyName(4, "orange.png");
+            this.imageList2.Images.SetKeyName(5, "pinapple.jpg");
+            this.imageList2.Images.SetKeyName(6, "strawberry.jpg");
+            this.imageList2.Images.SetKeyName(7, "watermelon.jpg");
+            // 
+            // frm_SnakesAndLaddersBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 648);
+            this.Controls.Add(this.pbx_Diceface);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lbl_PlayerThatIsGoing);
+            this.Controls.Add(this.lbl_winnerIs);
+            this.Controls.Add(this.lbl_winnerTitle);
+            this.Controls.Add(this.btn_rollDie);
+            this.Controls.Add(this.pnl_board);
+            this.Name = "frm_SnakesAndLaddersBoard";
+            this.Text = "Snakes And Ladders";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Diceface)).EndInit();
+            this.pnl_board.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Player1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_board;
+        private System.Windows.Forms.Button btn_rollDie;
+        private System.Windows.Forms.PictureBox pbx_Player2;
+        private System.Windows.Forms.PictureBox pbx_Player1;
+        private System.Windows.Forms.Label lbl_winnerTitle;
+        private System.Windows.Forms.Label lbl_winnerIs;
+        private System.Windows.Forms.Label lbl_PlayerThatIsGoing;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.PictureBox pbx_Diceface;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+    }
+}
+
---
+++ Snakes And Ladders/Snakes And Ladders/frm_SnakesAndLaddersBoard.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snakes_And_Ladders
+{
+    public partial class frm_SnakesAndLaddersBoard : Form
+    {
+        //-------------------------------------Declaration of Global Variables-------------------------------------\\
+
+        //Seperate arrays for each player so that finding positions is correct
+        Point[] boardSqauresForPlayer1 = new Point[101];
+        Point[] boardSqauresForPlayer2 = new Point[101];
+
+        //Using board positions
+        int[] snakeHeads =new int[]{17, 54, 62, 64, 87, 93, 95, 98};
+        int[] snakeEnds = new int[]{ 7, 34, 19, 60, 24, 73, 75, 79};
+        int[] ladderStarts = new int[] { 1, 4, 9, 21, 28, 51, 71, 80};
+        int[] ladderEnds = new int[] { 38, 14, 31, 42, 84, 67, 91, 100};
+
+        int boardPositionPlayer1 = 0;
+        int boardPositionPlayer2 = 0;
+        int turn = 1;
+
+        //-------------------------------------End Declaration of Global Variables-------------------------------------\\
+
+
+
+        public frm_SnakesAndLaddersBoard()
+        {
+            InitializeComponent();            
+        }
+
+
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            //Turn invisible
+            btn_Start.Visible = false;
+
+            //Set up point array
+            SetUpBoard(ref boardSqauresForPlayer1, ref boardSqauresForPlayer2);
+
+            //Inform of whose turn it is
+            lbl_PlayerThatIsGoing.Text = "Player 1's turn";
+
+            //Enable die rolling ability
+            btn_rollDie.Enabled = true;
+
+        }//End Start Button click
+
+
+
+        //sets up the point array for the board
+        public static void SetUpBoard(ref Point[] boardSqauresForPlayer1, ref Point[] boardSqauresForPlayer2)
+        {
+            //-------------------------------Declaration Of Variables-------------------------------\\
+            
+            int changeInBoardPosition = 60;
+            int startOfRow = 1;
+            int endOfRow = 11;
+            int xBoard = -35;
+            int yBoardPlayer1 = 538;
+            int yBoardPlayer2 = 564;
+
+            //-------------------------------Declaration Of Variables-------------------------------\\
+
+            //Starting position added to the arrays
+            boardSqauresForPlayer1[0] = new Point { X = xBoard, Y = yBoardPlayer1 };
+            boardSqauresForPlayer2[0] = new Point { X = xBoard, Y = yBoardPlayer2 };
+
+            //Set up first row
+            setUpRow(ref boardSqauresForPlayer1, ref boardSqauresForPlayer2, changeInBoardPosition, startOfRow, endOfRow, ref xBoard, yBoardPlayer1, yBoardPlayer2);
+
+            for (int rowNumber = 2; rowNumber < 11; rowNumber++)
+            {
+                if (rowNumber % 2 != 0)
+                {
+                    //If the row is even it must be going from left to right i.e to the right
+                    SetUpToTheLeft(ref boardSqauresForPlayer1, ref boardSqauresForPlayer2, ref changeInBoardPosition, ref startOfRow, ref endOfRow, ref xBoard, ref yBoardPlayer1, ref yBoardPlayer2);
+                }
+                else
+                {
+                    //If the row is odd it must be going from right to left i.e to the left
+                    SetUpToTheRight(ref boardSqauresForPlayer1, ref boardSqauresForPlayer2, ref changeInBoardPosition, ref startOfRow, ref endOfRow, ref xBoard, ref yBoardPlayer1, ref yBoardPlayer2);
+
+                }//End row number even if
+
+            }//End Row setup for
+                       
+
+        }//End SetUpBoard
+
+
+
+        private static void SetUpToTheLeft(ref Point[] boardSqauresForPlayer1, ref Point[] boardSqauresForPlayer2, ref int changeInBoardPosition, ref int startOfRow, ref int endOfRow, ref int xBoard, ref int yBoardPlayer1, ref int yBoardPlayer2)
+        {
+            //Set new y values for each player and the increment amount
+            yBoardPlayer1 = yBoardPlayer1 - 58;
+            yBoardPlayer2 = yBoardPlayer2 - 58;
+            changeInBoardPosition = 60;
+            startOfRow = endOfRow + 1;
+            endOfRow = endOfRow + 10;
+
+            //Set up the first square of the third row
+            boardSqauresForPlayer1[startOfRow - 1] = new Point { X = xBoard, Y = yBoardPlayer1 };
+            boardSqauresForPlayer2[startOfRow - 1] = new Point { X = xBoard, Y = yBoardPlayer2 };
+
+            //Set up third row
+            setUpRow(ref boardSqauresForPlayer1, ref boardSqauresForPlayer2, changeInBoardPosition, startOfRow, endOfRow, ref xBoard, yBoardPlayer1, yBoardPlayer2);
+        }
+
+
+
+        private static void SetUpToTheRight(ref Point[] boardSqauresForPlayer1, ref Point[] boardSqauresForPlayer2, ref int changeInBoardPosition, ref int startOfRow, ref int endOfRow, ref int xBoard, ref int yBoardPlayer1, ref int yBoardPlayer2)
+        {
+            //Set new y values for each player and the increment amount
+            yBoardPlayer1 = yBoardPlayer1 - 58;
+            yBoardPlayer2 = yBoardPlayer2 - 58;
+            changeInBoardPosition = -60;
+            startOfRow = endOfRow + 1;
+            endOfRow = endOfRow + 10;
+
+            //Set up the first square of the second row
+            boardSqauresForPlayer1[startOfRow - 1] = new Point { X = xBoard, Y = yBoardPlayer1 };
+            boardSqauresForPlayer2[startOfRow - 1] = new Point { X = xBoard, Y = yBoardPlayer2 };
+
+            //Set up second row
+            setUpRow(ref boardSqauresForPlayer1, ref boardSqauresForPlayer2, changeInBoardPosition, startOfRow, endOfRow, ref xBoard, yBoardPlayer1, yBoardPlayer2);
+        }
+
+
+        //Sets up the point array for that row
+        private static void setUpRow(ref Point[] boardSqauresForPlayer1, ref Point[] boardSqauresForPlayer2, int changeInBoardPosition, int startOfRow, int endOfRow, ref int xBoard, int yBoardPlayer1, int yBoardPlayer2)
+        {
+
+            for (int boardPosition = startOfRow; boardPosition < endOfRow; boardPosition++)
+            {
+                //Increment the x co-ordinate
+                xBoard = xBoard + changeInBoardPosition;
+
+                //Position added to the arrays
+                boardSqauresForPlayer1[boardPosition] = new Point { X = xBoard, Y = yBoardPlayer1 };
+                boardSqauresForPlayer2[boardPosition] = new Point { X = xBoard, Y = yBoardPlayer2 };
+
+            }            
+
+        }//End SetUpRow
+
+
+
+        private void btn_rollDie_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();         
+
+            int diceRoll = rnd.Next(1, 7);
+
+
+            switch (diceRoll)
+            {
+                case 1:
+                    pbx_Diceface.Image = imageList2.Images[diceRoll - 1];
+                    break;
+
+                case 2:
+                    pbx_Diceface.Image = Snakes_And_Ladders.Properties.Resources.diceFace2;
+                    break;
+
+                case 3:
+                    pbx_Diceface.Image = Snakes_And_Ladders.Properties.Resources.diceFace3;
+                    break;
+
+                case 4:
+                    pbx_Diceface.Image = Snakes_And_Ladders.Properties.Resources.diceFace4;
+                    break;
+
+                case 5:
+                    pbx_Diceface.Image = Snakes_And_Ladders.Properties.Resources.diceFace5;
+                    break;
+
+                case 6:
+                    pbx_Diceface.Image = Snakes_And_Ladders.Properties.Resources.diceFace6;
+                    break;
+
+            }
+
+            //If the first time the player moves
+            if (turn == 1)
+            {
+                pbx_Player1.Visible = true;
+            }
+            else if (turn == 2)
+            {
+                pbx_Player2.Visible = true;
+            }
+            
+
+            //if odd turn first player else second player
+            if (turn % 2 != 0)
+            {
+                //Increase board Position for player 1
+                boardPositionPlayer1 = boardPositionPlayer1 + diceRoll;
+
+                ////If player is at 100 or 80 (because 80 has a ladder to 100 which is win condition)
+                if (boardPositionPlayer1 >= 100 || boardPositionPlayer1 == 80)
+                {
+                    //make it apparant that a player won
+                    WinnerLabel(" 1");
+                }
+                else
+                {
+                    //Check board position i.e Check If On a Snake Or a Ladder
+                    boardPositionPlayer1 = CheckIfOnSnakeOrLadder(boardPositionPlayer1, snakeHeads, snakeEnds, ladderStarts, ladderEnds);
+
+                    //Increase to board position
+                    pbx_Player1.Location = boardSqauresForPlayer1[boardPositionPlayer1];
+
+                    //Increase turn
+                    turn = turn + 1;
+
+                    lbl_PlayerThatIsGoing.Text = "Player 2's turn";
+
+                }//End Player 1 win or move if statement
+
+            }
+            else
+            {
+                //Increase board Position for player 2
+                boardPositionPlayer2 = boardPositionPlayer2 + diceRoll;
+
+                //If player is at 100 or 80 (because 80 has a ladder to 100 which is win condition)
+                if (boardPositionPlayer2 >= 100 || boardPositionPlayer2 == 80)
+                {
+                    //make it apparant that a player won
+                    WinnerLabel(" 2");
+                }
+                else
+                {
+                    //Check board position i.e Check If On a Snake Or a Ladder
+                    boardPositionPlayer2 = CheckIfOnSnakeOrLadder(boardPositionPlayer2, snakeHeads, snakeEnds, ladderStarts, ladderEnds);
+
+                    //Increase to board position
+                    pbx_Player2.Location = boardSqauresForPlayer2[boardPositionPlayer2];
+
+                    //Increase turn
+                    turn = turn + 1;
+
+                    lbl_PlayerThatIsGoing.Text = "Player 1's turn";
+
+                }//End Player 2 win or move if statement
+
+            }//End odd turn if statement
+
+        }//End btn_rollDie_Click
+
+
+
+        private static int CheckIfOnSnakeOrLadder(int boardPosition, int[] snakeHeads, int[] snakeEnds, int[] ladderStarts, int[] ladderEnds)
+        {
+            /*
+             * for all elements in snake head array
+                 * check if board position is held in snake heads array
+                 * if true return position
+             * for all elements in ladder starts array
+                 * if board position is held in ladder starts array
+                 * if true return position
+             */
+
+            //for all elements in snake head array
+            for (int arrayPosition = 0; arrayPosition < snakeHeads.Length; arrayPosition++)
+            {
+                //check if board position is held in snake heads array
+                if (boardPosition == snakeHeads[arrayPosition])
+                {
+                    //inform of their misforutune
+                    MessageBox.Show("You hit a snake.\nHAHA");
+
+                    //if true return position
+                    return snakeEnds[arrayPosition];
+                    
+
+                }//End if
+
+            }//End for
+
+
+            //for all elements in ladder starts array
+            for (int arrayPosition = 0; arrayPosition < ladderStarts.Length; arrayPosition++)
+            {
+                //if board position is held in ladder starts array
+                if (boardPosition == ladderStarts[arrayPosition])
+                {
+                    //inform of their forutune
+                    MessageBox.Show("You went up a ladder.\nCongratulations!");
+
+                    //if true return position
+                    return ladderEnds[arrayPosition];
+
+                }//End if
+
+            }//End for
+
+            //if not included
+            return boardPosition;
+
+        }//End CheckIfOnSnakeOrLadder
+
+
+
+        private void WinnerLabel(string whoWon)
+        {
+            //Make winner labels visible
+            lbl_winnerTitle.Visible = true;
+            lbl_winnerIs.Visible = true;
+            lbl_winnerIs.Text = lbl_winnerIs.Text + whoWon;
+            btn_rollDie.Enabled = false;
+
+        }//End WinnerLabel
+        
+    }
+}
